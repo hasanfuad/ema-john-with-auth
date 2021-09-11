3f3fe2bc8a1c0603a4ce90e8d3f919f76f36497c
hasanfuad <hasanmdfuad@hotmail.com>
2021-09-11 12:27:06 +0600

    data fetched from database in the shop

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import fakeData from "../../fakeData";
 
 const Inventory = () => {
-    return (
-        <div>
-            <h1>Inventory coming soon...</h1>
-        </div>
-    );
+  const handleProduct = () => {
+    fetch('http://localhost:5000/addProducts',{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(fakeData)
+    }
+    )
+  };
+
+  
+  return (
+    <div>
+      <button onClick={handleProduct}>Add Product</button>
+      
+    </div>
+  );
 };
 
 export default Inventory;
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDrnTlaFOThHGej0ZkIglLffFZZioNSPiI",
-    authDomain: "ema-test-jkr.firebaseapp.com",
-    databaseURL: "https://ema-test-jkr.firebaseio.com",
-    projectId: "ema-test-jkr",
-    storageBucket: "ema-test-jkr.appspot.com",
-    messagingSenderId: "273630571694",
-    appId: "1:273630571694:web:dd5435e9e8b7deb372913f"
-  };
+  apiKey: "AIzaSyC7n4qZLwg7SE7krahkOfKGzAWms5sAwJM",
+  authDomain: "ema-john-online-shopping.firebaseapp.com",
+  projectId: "ema-john-online-shopping",
+  storageBucket: "ema-john-online-shopping.appspot.com",
+  messagingSenderId: "692247096126",
+  appId: "1:692247096126:web:3723de83acc4310cb81a6f"
+};
 export default firebaseConfig;
